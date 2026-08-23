75d91b2efe55d4ea51ae2c593b8f48e676ce1b88
Emirhan CAMCI <byemir@live.com>
2026-08-23 11:44:08 +0300

    Delete ios directory

--- ios/BackgroundFileTransfer.h
+++
@@ -1,8 +0,0 @@
-#import <BackgroundFileTransferSpec/BackgroundFileTransferSpec.h>
-#import <React/RCTEventEmitter.h>
-
-@interface BackgroundFileTransfer : RCTEventEmitter <NativeBackgroundFileTransferSpec, NSURLSessionDownloadDelegate, NSURLSessionDataDelegate>
-
-@property (nonatomic, strong) NSURLSession *session;
-
-@end
